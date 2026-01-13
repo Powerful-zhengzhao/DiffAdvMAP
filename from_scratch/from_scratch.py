35ffd5dc131401fd01432461af228ca7a31a2c22
zhengzhao-pan <zhengzhao.pan@gmail.com>
2026-01-13 15:56:22 +0800

    add from_scratch

---
+++ from_scratch/from_scratch.py
@@ -0,0 +1,134 @@
+import sys
+import os 
+import random
+import argparse
+
+sys.path.append(".")
+sys.path.append('./taming-transformers')
+
+import torch
+from omegaconf import OmegaConf
+
+from ldm.util import instantiate_from_config
+
+from ldm.models.diffusion.ddim_adv import DDIMSampler
+from torchvision import models
+
+from torch.backends import cudnn
+
+import numpy as np
+from torchvision.transforms import ToPILImage
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--batch-size', type=int, default=1)
+parser.add_argument('--seed', type=int, default=0)
+parser.add_argument('--scale', type=float, default=3.0)
+parser.add_argument('--ddim-steps', type=int, default=200)
+parser.add_argument('--ddim-eta', type=float, default=0.0)
+parser.add_argument('--mask', type=bool, default=True)
+parser.add_argument('--save-dir', type=str, default='../images/imagenetsc/')
+parser.add_argument('--target-model', type=str, default="Pure_Resnet50")
+args = parser.parse_args()
+
+def load_model_from_config(config, ckpt):
+    print(f"Loading model from {ckpt}")
+    pl_sd = torch.load(ckpt)#, map_location="cpu")
+    sd = pl_sd["state_dict"]
+    model = instantiate_from_config(config.model)
+    m, u = model.load_state_dict(sd, strict=False)
+    model.cuda()
+    model.eval()
+    return model
+
+def normalize(image, shape=(256, 256)):
+    image = np.array(image.convert("RGB").resize(shape))
+    image = image.astype(np.float32) / 255.0
+    image = image[None].transpose(0, 3, 1, 2)
+    image = torch.from_numpy(image)
+    image = image * 2.0 - 1.0
+    return image
+
+def prepare_target_model(target_model, device):
+    if target_model=='Pure_Resnet50':
+        print("loading Pure Resnet50")
+        model = models.resnet50(pretrained=True).to(device).eval()
+    elif target_model == 'mobilenet-v2':
+        print("loading mobilenet")
+        model = models.mobilenet_v2(pretrained=True).to(device).eval()
+    elif target_model == 'inception-v3':
+        print("loading inception")
+        model = models.inception_v3(pretrained=True).to(device).eval()
+    elif target_model == 'swin-transformer':
+        print("loading swin-transformer")
+        model = models.swin_b(weights=models.Swin_B_Weights.IMAGENET1K_V1)
+        model.to(device).eval()
+    elif target_model == 'vgg':
+        print("loading vgg")
+        model = models.vgg19(pretrained=True)
+        model.to(device).eval()
+    else:
+        raise NotImplementedError("No such model!")
+    return model
+
+def get_model():
+    config = OmegaConf.load("configs/latent-diffusion/cin256-v2.yaml")  
+    model = load_model_from_config(config, "models/ldm/cin256-v2/model.ckpt")
+    return model
+
+
+def main():
+    device = torch.cuda.current_device() if torch.cuda.is_available() else "cpu"
+    cudnn.benchmark = False
+    cudnn.deterministic = True
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+
+    outpath = os.path.join(args.save_dir, args.target_model)+'/samples'
+    os.makedirs(outpath, exist_ok=True)
+    
+    model = get_model()
+    vic_model = prepare_target_model(args.target_model, device)
+    sampler = DDIMSampler(model, vic_model=vic_model)
+    n_samples_per_class = args.batch_size
+
+    ddim_steps = args.ddim_steps
+    ddim_eta = args.ddim_eta
+    scale = args.scale   # for unconditional guidance
+
+    classes =  np.arange(1000)
+    with torch.no_grad():
+        with model.ema_scope():
+            uc = model.get_learned_conditioning(
+                {model.cond_stage_key: torch.tensor(n_samples_per_class*[1000]).to(model.device)}
+                )
+
+            for class_label in classes:
+                print(f"rendering {n_samples_per_class} examples of class '{class_label}' in {ddim_steps} steps and using s={scale:.2f}.")
+                xc = torch.tensor(n_samples_per_class*[class_label], dtype=torch.long)
+                c = model.get_learned_conditioning({model.cond_stage_key: xc.to(model.device)})
+
+                samples_ddim, _ = sampler.sample(S=ddim_steps,
+                                                 conditioning=c,
+                                                 batch_size=n_samples_per_class,
+                                                 shape=[3, 64, 64],
+                                                 verbose=False,
+                                                 unconditional_guidance_scale=scale,
+                                                 unconditional_conditioning=uc,
+                                                 eta=ddim_eta,
+                                                 label=xc.to(model.device)
+                                                 )
+
+                x_samples_ddim = model.decode_first_stage(samples_ddim)
+                x_samples_ddim = torch.clamp((x_samples_ddim+1.0)/2.0,
+                                             min=0.0, max=1.0)
+                for i in range(args.batch_size):
+                    img_name = str(class_label)+'_'+str(i)+'.png'
+                    pil = ToPILImage()(x_samples_ddim[i,:,:,:])
+                    pil.save(os.path.join(outpath, img_name))
+
+            
+if __name__ == '__main__':
+    main()
---
+++ from_scratch/ldm/models/diffusion/ddim_adv.py
@@ -0,0 +1,333 @@
+"""SAMPLING ONLY."""
+
+import torch
+import numpy as np
+from tqdm import tqdm
+from functools import partial
+import torchvision.transforms as T
+
+from ldm.modules.diffusionmodules.util import make_ddim_sampling_parameters, make_ddim_timesteps, noise_like
+from torchvision.models import resnet50
+import torch.nn.functional as F
+from torchvision.utils import save_image, make_grid
+
+#weights = ResNet50_Weights.DEFAULT
+#preprocess = weights.transforms()
+
+preprocess = T.Compose([
+    T.Resize((256,256)),
+    T.CenterCrop((224,224)),
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+def get_target_label(logits, label, device): # seond-like label for attack
+    
+    rates, indices = logits.sort(1, descending=True) 
+    rates, indices = rates.squeeze(0), indices.squeeze(0)
+    tar_label = torch.zeros_like(label).to(device)
+    if label == indices[0]:
+        tar_label = indices[1]
+    else:
+        tar_label = indices[0]
+    # for i in range(label.shape[0]):
+    #     if label[i] == indices[i][0]:  # classify is correct
+    #         tar_label[i] = indices[i][1]
+    #     else:
+    #         tar_label[i] = indices[i][0]
+    
+    return tar_label
+
+
+class DDIMSampler(object):
+    def __init__(self, model, schedule="linear", vic_model=None, **kwargs):
+        
+        super().__init__()
+        self.model = model
+        self.ddpm_num_timesteps = model.num_timesteps
+        self.schedule = schedule
+        
+        self.vic_model = vic_model
+
+        
+
+
+    def register_buffer(self, name, attr):
+        if type(attr) == torch.Tensor:
+            if attr.device != torch.device("cuda"):
+                attr = attr.to(torch.device("cuda"))
+        setattr(self, name, attr)
+
+    def make_schedule(self, ddim_num_steps, ddim_discretize="uniform", ddim_eta=0., verbose=True):
+        self.ddim_timesteps = make_ddim_timesteps(ddim_discr_method=ddim_discretize, num_ddim_timesteps=ddim_num_steps,
+                                                  num_ddpm_timesteps=self.ddpm_num_timesteps,verbose=verbose)
+        alphas_cumprod = self.model.alphas_cumprod
+        assert alphas_cumprod.shape[0] == self.ddpm_num_timesteps, 'alphas have to be defined for each timestep'
+        to_torch = lambda x: x.clone().detach().to(torch.float32).to(self.model.device)
+
+        self.register_buffer('betas', to_torch(self.model.betas))
+        self.register_buffer('alphas_cumprod', to_torch(alphas_cumprod))
+        self.register_buffer('alphas_cumprod_prev', to_torch(self.model.alphas_cumprod_prev))
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        self.register_buffer('sqrt_alphas_cumprod', to_torch(np.sqrt(alphas_cumprod.cpu())))
+        self.register_buffer('sqrt_one_minus_alphas_cumprod', to_torch(np.sqrt(1. - alphas_cumprod.cpu())))
+        self.register_buffer('log_one_minus_alphas_cumprod', to_torch(np.log(1. - alphas_cumprod.cpu())))
+        self.register_buffer('sqrt_recip_alphas_cumprod', to_torch(np.sqrt(1. / alphas_cumprod.cpu())))
+        self.register_buffer('sqrt_recipm1_alphas_cumprod', to_torch(np.sqrt(1. / alphas_cumprod.cpu() - 1)))
+
+        # ddim sampling parameters
+        ddim_sigmas, ddim_alphas, ddim_alphas_prev = make_ddim_sampling_parameters(alphacums=alphas_cumprod.cpu(),
+                                                                                   ddim_timesteps=self.ddim_timesteps,
+                                                                                   eta=ddim_eta,verbose=verbose)
+        self.register_buffer('ddim_sigmas', ddim_sigmas)
+        self.register_buffer('ddim_alphas', ddim_alphas)
+        self.register_buffer('ddim_alphas_prev', ddim_alphas_prev)
+        self.register_buffer('ddim_sqrt_one_minus_alphas', np.sqrt(1. - ddim_alphas))
+        sigmas_for_original_sampling_steps = ddim_eta * torch.sqrt(
+            (1 - self.alphas_cumprod_prev) / (1 - self.alphas_cumprod) * (
+                        1 - self.alphas_cumprod / self.alphas_cumprod_prev))
+        self.register_buffer('ddim_sigmas_for_original_num_steps', sigmas_for_original_sampling_steps)
+
+    @torch.no_grad()
+    def sample(self,
+               S,
+               batch_size,
+               shape,
+               conditioning=None,
+               callback=None,
+               normals_sequence=None,
+               img_callback=None,
+               quantize_x0=False,
+               eta=0.,
+               temperature=1.,
+               noise_dropout=0.,
+               score_corrector=None,
+               corrector_kwargs=None,
+               verbose=True,
+               x_T=None,
+               log_every_t=100,
+               unconditional_guidance_scale=1.,
+               unconditional_conditioning=None,
+               label=None,reference=None, K=10,s=2,a=1,
+               # this has to come in the same format as the conditioning, # e.g. as encoded tokens, ...
+               **kwargs
+               ):
+        if conditioning is not None:
+            if isinstance(conditioning, dict):
+                cbs = conditioning[list(conditioning.keys())[0]].shape[0]
+                if cbs != batch_size:
+                    print(f"Warning: Got {cbs} conditionings but batch-size is {batch_size}")
+            else:
+                if conditioning.shape[0] != batch_size:
+                    print(f"Warning: Got {conditioning.shape[0]} conditionings but batch-size is {batch_size}")
+
+        self.make_schedule(ddim_num_steps=S, ddim_eta=eta, verbose=verbose)
+        # sampling
+        C, H, W = shape
+        size = (batch_size, C, H, W)
+        print(f'Data shape for DDIM sampling is {size}, eta {eta}')
+
+        samples, intermediates = self.ddim_sampling(conditioning, size,
+                                                    callback=callback,
+                                                    img_callback=img_callback,
+                                                    quantize_denoised=quantize_x0,
+                                                    ddim_use_original_steps=False,
+                                                    noise_dropout=noise_dropout,
+                                                    temperature=temperature,
+                                                    score_corrector=score_corrector,
+                                                    corrector_kwargs=corrector_kwargs,
+                                                    x_T=x_T,
+                                                    log_every_t=log_every_t,
+                                                    unconditional_guidance_scale=unconditional_guidance_scale,
+                                                    unconditional_conditioning=unconditional_conditioning,label=label, reference=reference,
+                                                    )
+        return samples, intermediates
+
+    @torch.no_grad()
+    def ddim_sampling(self, cond, shape,
+                      x_T=None, ddim_use_original_steps=False,
+                      callback=None, timesteps=None, quantize_denoised=False,
+                      img_callback=None, log_every_t=100,
+                      temperature=1., noise_dropout=0., score_corrector=None, corrector_kwargs=None,
+                      unconditional_guidance_scale=1., unconditional_conditioning=None,label=None,reference=None):
+        device = self.model.betas.device
+        b = shape[0]
+        if x_T is None:
+            img = torch.randn(shape, device=device)
+        else:
+            img = x_T
+
+        if timesteps is None:
+            timesteps = self.ddpm_num_timesteps if ddim_use_original_steps else self.ddim_timesteps
+        elif timesteps is not None and not ddim_use_original_steps:
+            subset_end = int(min(timesteps / self.ddim_timesteps.shape[0], 1) * self.ddim_timesteps.shape[0]) - 1
+            timesteps = self.ddim_timesteps[:subset_end]
+
+        intermediates = {'x_inter': [img], 'pred_x0': [img]}
+        time_range = reversed(range(0,timesteps)) if ddim_use_original_steps else np.flip(timesteps)
+        total_steps = timesteps if ddim_use_original_steps else timesteps.shape[0]
+
+
+        pri_img = img.detach().requires_grad_(True)
+        img_origin = img
+
+        img = pri_img.detach().requires_grad_(True)
+
+        print(f"Running DDIM Sampling with {total_steps} timesteps")
+
+        iterator = tqdm(time_range, desc='DDIM Sampler', total=total_steps)
+
+        # print(time_range[-40],img.shape)
+        for i, step in enumerate(iterator):
+            index = total_steps - i - 1
+            ts = torch.full((b,), step, device=device, dtype=torch.long)
+
+            outs = self.p_sample_ddim(img, cond, ts, index=index, use_original_steps=ddim_use_original_steps,
+                                      quantize_denoised=quantize_denoised, temperature=temperature,
+                                      noise_dropout=noise_dropout, score_corrector=score_corrector,
+                                      corrector_kwargs=corrector_kwargs,
+                                      unconditional_guidance_scale=unconditional_guidance_scale,
+                                      unconditional_conditioning=unconditional_conditioning,img_origin=img_origin, label=label,reference=reference)
+            img, pred_x0, img_origin = outs
+
+
+            '''
+            if index % 20 == 0:
+                x_samples_ddim = self.model.decode_first_stage(img)
+                x_samples_ddim = torch.clamp((x_samples_ddim+1.0)/2.0, 
+                                             min=0.0, max=1.0)
+                save_image(x_samples_ddim, f"img/Diff_{index}.png", nrow=1, normalize=True)
+            '''
+
+            if callback: callback(i)
+            if img_callback: img_callback(pred_x0, i)
+
+            if index % log_every_t == 0 or index == total_steps - 1:
+                intermediates['x_inter'].append(img)
+                intermediates['pred_x0'].append(pred_x0)
+
+                
+        return img, intermediates
+
+    @torch.no_grad()
+    def p_sample_ddim(self, x, c, t, index, repeat_noise=False, use_original_steps=False, quantize_denoised=False,
+                      temperature=1., noise_dropout=0., score_corrector=None, corrector_kwargs=None,
+                      unconditional_guidance_scale=1., unconditional_conditioning=None,img_origin=None, label=None, reference=None, const=-30,lr_xt=0.01):
+        b, *_, device = *x.shape, x.device
+        total_steps = self.ddim_timesteps.shape[0]
+        if unconditional_conditioning is None or unconditional_guidance_scale == 1.:
+            e_t = self.model.apply_model(x, t, c)
+            if reference is None and total_steps * 0 < index <= total_steps * 0.2:
+                e_t_origin = self.model.apply_model(img_origin, t, c)
+            else:
+                e_t_origin = e_t
+        else:
+            x_in = torch.cat([x] * 2)
+            t_in = torch.cat([t] * 2)
+            c_in = torch.cat([unconditional_conditioning, c])
+            e_t_uncond, e_t = self.model.apply_model(x_in, t_in, c_in).chunk(2)
+            e_t = e_t_uncond + unconditional_guidance_scale * (e_t - e_t_uncond)
+            if reference is None and total_steps * 0 <= index <= total_steps * 0.2:
+                x_in_origin = torch.cat([img_origin] * 2)
+                e_t_uncond_origin, e_t_origin = self.model.apply_model(x_in_origin, t_in, c_in).chunk(2)
+                e_t_origin = e_t_uncond_origin + unconditional_guidance_scale * (e_t_origin - e_t_uncond_origin)
+            else:
+                e_t_origin = e_t
+
+        if score_corrector is not None:
+            assert self.model.parameterization == "eps"
+            e_t = score_corrector.modify_score(self.model, e_t, x, t, c, **corrector_kwargs)
+            e_t_origin= score_corrector.modify_score(self.model, e_t_origin, img_origin, t, c, **corrector_kwargs)
+
+        alphas = self.model.alphas_cumprod if use_original_steps else self.ddim_alphas
+        alphas_prev = self.model.alphas_cumprod_prev if use_original_steps else self.ddim_alphas_prev
+        sqrt_one_minus_alphas = self.model.sqrt_one_minus_alphas_cumprod if use_original_steps else self.ddim_sqrt_one_minus_alphas
+        sigmas = self.model.ddim_sigmas_for_original_num_steps if use_original_steps else self.ddim_sigmas
+        # select parameters corresponding to the currently considered timestep
+        a_t = torch.full((b, 1, 1, 1), alphas[index], device=device)
+        a_prev = torch.full((b, 1, 1, 1), alphas_prev[index], device=device)
+        sigma_t = torch.full((b, 1, 1, 1), sigmas[index], device=device)
+        sqrt_one_minus_at = torch.full((b, 1, 1, 1), sqrt_one_minus_alphas[index],device=device)
+
+        # current prediction for x_0
+        pred_x0 = (x - sqrt_one_minus_at * e_t) / a_t.sqrt()
+        if reference is None and total_steps * 0 <= index <= total_steps * 0.2:
+            origin_pred_x0 = (img_origin - sqrt_one_minus_at * e_t_origin) / a_t.sqrt()
+        else:
+            origin_pred_x0 = pred_x0
+
+
+        if (index >= total_steps * 0 and index <= total_steps * 0.2):
+            sqrt_one_minus_at = torch.full((b, 1, 1, 1), sqrt_one_minus_alphas[index], device=device)
+            with torch.enable_grad():
+                origin_x = x.clone().detach()
+                x = x.detach().requires_grad_(True)
+
+                img_transformed_origin = self.model.differentiable_decode_first_stage(
+                    origin_pred_x0)  # image transformation from latent code
+                img_transformed_origin = torch.clamp((img_transformed_origin + 1.0) / 2.0,
+                                                     min=0.0, max=1.0)
+                img_transformed_origin = preprocess(img_transformed_origin).to(
+                    device)  # image transformation to model input
+                logits_origin = self.vic_model(img_transformed_origin)
+                probs = F.softmax(logits_origin)
+                prob = probs[0, label]
+                const = const - 10 * prob
+
+                for step in range(3):
+                    img_transformed = self.model.differentiable_decode_first_stage(
+                        pred_x0)  # image transformation from latent code
+                    img_transformed = torch.clamp((img_transformed + 1.0) / 2.0,
+                                                  min=0.0, max=1.0)
+                    img_transformed = preprocess(img_transformed).to(device)  # image transformation to model input
+
+
+                    logits = self.vic_model(img_transformed)
+                    temp = torch.eye(len(logits[0])).cuda()
+                    one_hot_labels = temp[label].to(pred_x0.device)
+                    i, _ = torch.max((1 - one_hot_labels) * logits, dim=1)
+                    j = torch.masked_select(logits, one_hot_labels.bool())
+
+                    ret = (const - (j - i)) ** 2 + torch.sum((origin_pred_x0 - pred_x0) ** 2)
+                    loss = ret+0.01*torch.sum((origin_x - x) ** 2)
+
+                    x_grad = torch.autograd.grad(
+                        loss, x, retain_graph=False, create_graph=False
+                    )[0].detach()
+                    print(torch.norm(x_grad, p=2))
+                    if torch.norm(x_grad, p=2).isnan():
+                        new_x = x
+                    else:
+                        new_x = x - lr_xt * x_grad
+                    x = new_x.detach().requires_grad_(True)
+                    if unconditional_conditioning is None or unconditional_guidance_scale == 1.:
+                        e_t = self.model.apply_model(x, t, c)
+                    else:
+                        x_in = torch.cat([x] * 2)
+                        t_in = torch.cat([t] * 2)
+                        c_in = torch.cat([unconditional_conditioning, c])
+                        e_t_uncond, e_t = self.model.apply_model(x_in, t_in, c_in).chunk(2)
+                        e_t = e_t_uncond + unconditional_guidance_scale * (e_t - e_t_uncond)
+                    pred_x0 = (x - sqrt_one_minus_at * e_t) / a_t.sqrt()
+                    del loss, x_grad
+                    torch.cuda.empty_cache()
+
+        if quantize_denoised:
+            pred_x0, _, *_ = self.model.first_stage_model.quantize(pred_x0)
+            if reference is None and total_steps * 0 <= index <= total_steps * 0.2:
+                origin_pred_x0, _, *_ = self.model.first_stage_model.quantize(origin_pred_x0)
+            else:
+                origin_pred_x0 = pred_x0
+        # direction pointing to x_t
+        dir_xt = (1. - a_prev - sigma_t**2).sqrt() * e_t
+        if reference is None and total_steps * 0 <= index <= total_steps * 0.2:
+            dir_xt_origin = (1. - a_prev - sigma_t**2).sqrt() * e_t_origin
+        else:
+            dir_xt_origin = dir_xt
+        noise = sigma_t * noise_like(x.shape, device, repeat_noise) * temperature
+        if noise_dropout > 0.:
+            noise = torch.nn.functional.dropout(noise, p=noise_dropout)
+        x_prev = a_prev.sqrt() * pred_x0 + dir_xt + noise
+        img_origin = a_prev.sqrt() * origin_pred_x0 + dir_xt_origin + noise
+        return x_prev, pred_x0, img_origin
+
